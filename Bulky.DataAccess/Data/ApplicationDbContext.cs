9a4a2631c0c3748de98f882c9fded5bf1ae5b525
Andrew Storrow <andrew.storrow@gmail.com>
2023-04-26 11:59:12 -0500

    Add product model and data repository

--- Bulky.DataAccess/Data/ApplicationDbContext.cs
+++ Bulky.DataAccess/Data/ApplicationDbContext.cs
@@ -11,6 +11,7 @@ namespace Bulky.DataAccess.Data
         }
 
         public DbSet<Category> Categories { get; set; }
+        public DbSet<Product> Products { get; set; }
 
         protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
@@ -18,6 +19,45 @@ namespace Bulky.DataAccess.Data
                 new Category { Id = 1, Name = "Action", DisplayOrder = 1 },
                 new Category { Id = 2, Name = "SciFi", DisplayOrder = 2 },
                 new Category { Id = 3, Name = "History", DisplayOrder = 3 }
+                );
+
+            modelBuilder.Entity<Product>().HasData(
+                new Product
+                {
+                    Id = 1,
+                    Title = "Harry Potter and the Philosopher's Stone",
+                    Description = "Follows the story of Harry Potter, a young wizard who discovers his magical heritage on his eleventh birthday, when he receives a letter of acceptance to Hogwarts School of Witchcraft and Wizardry",
+                    ISBN = "978-0-7475-3269-9",
+                    Author = "J. K. Rowling",
+                    ListPrice = 12.99,
+                    Price = 9.99,
+                    Price50 = 8.99,
+                    Price100 = 8.49
+                },
+                new Product
+                {
+                    Id = 2,
+                    Title = "The Testament",
+                    Description = "Troy Phelan, an eccentric, reclusive, ruthless billionaire businessman, commits suicide. In order to cut his family out of his will, he makes a fake will a few hours before his suicide, putting his family into that will.",
+                    ISBN = "0-09-924502-7",
+                    Author = "John Grisham",
+                    ListPrice = 13.99,
+                    Price = 10.99,
+                    Price50 = 9.99,
+                    Price100 = 8.99
+                },
+                new Product
+                {
+                    Id = 3,
+                    Title = "Timeline",
+                    Description = "A group of history students travel to 14th-century France to rescue their professor.",
+                    ISBN = "0679444815",
+                    Author = "Michael Crichton",
+                    ListPrice = 11.99,
+                    Price = 10.99,
+                    Price50 = 9.99,
+                    Price100 = 8.99
+                }
                 );
         }
     }
---
+++ Bulky.DataAccess/Migrations/20230426165134_AddProductTableToDb.cs
@@ -0,0 +1,53 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+#pragma warning disable CA1814 // Prefer jagged arrays over multidimensional
+
+namespace Bulky.DataAccess.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddProductTableToDb : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Products",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Title = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Description = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    ISBN = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Author = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    ListPrice = table.Column<double>(type: "float", nullable: false),
+                    Price = table.Column<double>(type: "float", nullable: false),
+                    Price50 = table.Column<double>(type: "float", nullable: false),
+                    Price100 = table.Column<double>(type: "float", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Products", x => x.Id);
+                });
+
+            migrationBuilder.InsertData(
+                table: "Products",
+                columns: new[] { "Id", "Author", "Description", "ISBN", "ListPrice", "Price", "Price100", "Price50", "Title" },
+                values: new object[,]
+                {
+                    { 1, "J. K. Rowling", "Follows the story of Harry Potter, a young wizard who discovers his magical heritage on his eleventh birthday, when he receives a letter of acceptance to Hogwarts School of Witchcraft and Wizardry", "978-0-7475-3269-9", 12.99, 9.9900000000000002, 8.4900000000000002, 8.9900000000000002, "Harry Potter and the Philosopher's Stone" },
+                    { 2, "John Grisham", "Troy Phelan, an eccentric, reclusive, ruthless billionaire businessman, commits suicide. In order to cut his family out of his will, he makes a fake will a few hours before his suicide, putting his family into that will.", "0-09-924502-7", 13.99, 10.99, 8.9900000000000002, 9.9900000000000002, "The Testament" },
+                    { 3, "Michael Crichton", "A group of history students travel to 14th-century France to rescue their professor.", "0679444815", 11.99, 10.99, 8.9900000000000002, 9.9900000000000002, "Timeline" }
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Products");
+        }
+    }
+}
